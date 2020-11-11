a618c8272a01cc87ae17ca5cbe8d3e31953a876b
ross-u <cirkovic.uros.srb@gmail.com>
2020-11-11 01:43:53 +0100

    Minor update

---
+++ views/BeerDetails.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Layout = require('./Layout');
+
+const BeerCard = require('./components/BeerCard');
+
+// Iteration 4.2
+function BeerDetails(props) {
+  return (
+    <Layout>
+      <BeerCard beer={props.oneBeer} />
+    </Layout>
+  );
+}
+
+module.exports = BeerDetails;
